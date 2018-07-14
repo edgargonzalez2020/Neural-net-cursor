5dcd75b3bc4d587a5bcd5e3d4b31a340fb1a4f8c
Eddie_g <edgar.gonzalez2@mavs.uta.edu>
2018-07-13 21:45:36 -0500

    Deleted files.

--- src/sketch.js
+++ src/sketch.js
@@ -1,6 +1,26 @@
 let brain;
 let canvas;
 let point;
+class Circle {
+    constructor(){
+        this.x = height/2;
+        this.y = width/2;
+    }
+    getX(){
+        return this.x;
+    }
+    getY() {
+        return this.y;
+    }
+    update(newX,newY){
+        this.x = newX;
+        this.y = newY;
+    }
+    show(){
+        fill(255);
+        ellipse(this.x,this.y,10,10);
+    }
+}
 function setup() {
     brain = new NeuralNet();
     brain.buildNeuralNet();
